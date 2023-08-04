ffcebd899e5202e5af7d0c421976148d02eafcff
Carlos Eduardo <cadu.ce3@gmail.com>
2023-08-04 18:10:40 -0300

    a

--- portifolio-cadu-lucena/src/app/page.js
+++ portifolio-cadu-lucena/src/app/page.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header/Header'
 import Main from '../components/Main/Main'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
--- portifolio-cadu-lucena/src/components/Main/Main.js
+++ portifolio-cadu-lucena/src/components/Main/Main.js
@@ -1,9 +1,17 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './style.css';
 import { HiLocationMarker} from 'react-icons/hi' ;
 
-function Main() {
-  return (
+function Main(args) {
+
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);  
+  
+return (
+
     <>
         <section id='home'>
             <div>
@@ -13,7 +21,25 @@ function Main() {
                     <HiLocationMarker/>
                     Natal, Rio Grande do Norte, Brasil.
                 </div>
-                <button>Resumo</button>
+                <Button id='btn-resumo' onClick={toggle}>Resumo</Button>
+                <Modal isOpen={modal} toggle={toggle} {...args}id='modal-modal' >
+                    <ModalHeader id='titulo-modal' >Um pouco sobre mim...</ModalHeader>
+                    <ModalBody id='modal-body'>
+                        Fala galera, meu nome é Cadu, tenho 23 anos e sou natural de Natal, Rio Grande do Norte, Brasil. 
+                        Um dev não só apaixonado por tecnologia mas também por esporte. <br/> <br/>
+                        
+                        Sou um Desenvolvedor FrontEnd com +2 anos de experiência nesse área e minha jornada começou quando estagiei no Tribunial Regional do Trabalho da 21ª Região como desenvolvedor. <br/> <br/>
+
+                        Ao longo dos anos, tive a oportunidade de trabalhar em diversos projetos motivados, o que me iniciou um crescimento profissional e pessoal significativo. <br/> <br/>
+
+                        Atualmente sou graduando em Ciência da computação na UERN/Natal e bolsista de graduação na UFRN pelo IMD atuando como desenvolvedor frontEnd Web no projeto <a href="https://www.imd.ufrn.br/portal/noticias/6379/sistema-para-vigil%C3%A2ncia-epidemiol%C3%B3gica-desenvolvido-pelo-imd-entra-em-atividade-no-rn" target='_blank'>SUVEPI</a>. <br/> <br/>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button  id='btn-modal' onClick={toggle}>
+                            Voltar
+                        </Button>
+                    </ModalFooter>
+                </Modal>
             </div>
         </section>
     </>
