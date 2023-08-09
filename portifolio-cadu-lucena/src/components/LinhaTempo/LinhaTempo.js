dddfc1fe48706f90fbbc9397b8f703ac44079f79
Carlos Eduardo <cadu.ce3@gmail.com>
2023-08-08 21:03:58 -0300

    timeline

--- portifolio-cadu-lucena/src/components/LinhaTempo/LinhaTempo.js
+++ portifolio-cadu-lucena/src/components/LinhaTempo/LinhaTempo.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
+import Image from 'next/image';
 
 function LinhaTempo() {
   const timelineData = [
-    { date: '2019', description: 'Aqui foi onde tudo começou, foi onde tive a oportunidade de após fazer o ENEM, ingressar na Universidade do Estado do Rio Grande do Norte aqui em Natal.' },
-    { date: '2020', description: 'Descrição do evento 2' },
-    { date: '2021', description: 'Descrição do evento 3' },
-    { date: '2022', description: 'Descrição do evento 4' },
-    { date: '2023', description: 'Descrição do evento 5' },
+    { date: '2019', description: 'Aqui foi onde tudo começou, em 2019 tive a oportunidade de após ser aprovado no ENEM, ingressar na Universidade do Estado do Rio Grande do Norte aqui em Natal e cursar Ciência da Computação.', imagem:'/facul.png' },
+    { date: '2020', description: 'No ano de 2020 veio a pandemia, porém as aulas ficaram remotas para não sermos completamente prejudicados, foquei nos estudos!', imagem:'/estudos.png' },
+    { date: '2021', description: 'Em setembro de 2021 tive a oportunidade do meu primeiro estágio como desenvolvedor, ao lado de pessoas incríveis que agradeço até hoje pelo carinho e paciência comigo, aqui aprendi muito e foi onde dei os meus primeiros passos na área do desenvolvimento.', imagem:'/trt21.jpg' },
+    { date: '2022', description: 'Logo em seguida', imagem:'/suvepi.png' },
+    { date: '2023', description: 'Descrição do evento 5', imagem:'/eu1.png' },
   ];
 
   const [activeIndex, setActiveIndex] = useState(0);
@@ -54,6 +55,7 @@ function LinhaTempo() {
           <div className={`section-content ${activeIndex <= timelineData.length - 1 ? 'active' : ''}`}>
             <h4>{timelineData[activeIndex].date}</h4>
             <p>{timelineData[activeIndex].description}</p>
+            <Image id='img-card-anos' src={timelineData[activeIndex].imagem} width={450} height={250}/>
           </div>
         </div>
       </div>
