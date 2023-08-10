28df847179485ce69bf7a08c5fb52c7a6aca6b96
Carlos Eduardo <cadu.ce3@gmail.com>
2023-08-10 01:45:39 -0300

    ajustando o sobre do main

--- portifolio-cadu-lucena/src/components/Main/Main.js
+++ portifolio-cadu-lucena/src/components/Main/Main.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './style.css';
 import { HiLocationMarker} from 'react-icons/hi' ;
+import { MdGames} from 'react-icons/md' ;
+import { BsPersonWorkspace, BsArrowThroughHeartFill, BsBookHalf} from 'react-icons/bs' ;
+import { GrPersonalComputer} from 'react-icons/gr' ;
+import { AiOutlineEnvironment} from 'react-icons/ai' ;
+import { BiSolidBabyCarriage} from 'react-icons/bi' ;
 import Image from 'next/image';
 import LinhaTempo from '../LinhaTempo/LinhaTempo';
 import LineTecnologias from '../LineTecnologias/LineTecnologias';
@@ -38,7 +43,15 @@ return (
                 <Modal isOpen={modal} toggle={toggle} {...args}id='modal-modal' >
                     <ModalHeader id='titulo-modal' >Um pouco sobre mim...</ModalHeader>
                     <ModalBody id='modal-body'>
-                        Fala galera
+                        <ul>
+                            <li><BsPersonWorkspace className='icons-resumo'/>#OPEN_TO_WORK</li>
+                            <li><GrPersonalComputer className='icons-resumo'/>Desenvolvedor FrontEnd</li>
+                            <li><BiSolidBabyCarriage className='icons-resumo'/>23 anos.</li> 
+                            <li><AiOutlineEnvironment className='icons-resumo'/>Natal, Rio Grande do Norte, Brasil.</li> 
+                            <li><BsBookHalf className='icons-resumo'/>Graduando em Ciência da Computação pela UERN.</li> 
+                            <li><MdGames className='icons-resumo'/>Hobbys: Futebol, Games e Programação.</li> 
+                            <li><BsArrowThroughHeartFill className='icons-resumo'/>Em um relacionamento sério.</li> 
+                        </ul>
                     </ModalBody>
                     <ModalFooter>
                         <Button  id='btn-modal' onClick={toggle}>
