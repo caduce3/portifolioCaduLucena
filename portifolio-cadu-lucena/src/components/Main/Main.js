cbe0e390cea43f8ed04c1c8a9038c49d6ec499a0
Carlos Eduardo <cadu.ce3@gmail.com>
2023-08-11 21:19:40 -0300

    Finalizando section dos projetos pessoais

--- portifolio-cadu-lucena/src/components/Main/Main.js
+++ portifolio-cadu-lucena/src/components/Main/Main.js
@@ -155,7 +155,7 @@ return (
             
                 initial={{ opacity: 0, y: 500 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{  duration: 0.5, delay: 1  }}
+                transition={{  duration: 0.5, delay: 0.5  }}
             >
                 <a href="https://www.imd.ufrn.br/portal/noticias/6379/sistema-para-vigil%C3%A2ncia-epidemiol%C3%B3gica-desenvolvido-pelo-imd-entra-em-atividade-no-rn" target='_blank'>
                     <CardTrabalhosProjetos caminhoImagem={'/suvepi.png'} titulo='SUVEPI, Parceria IMD e SESAP/RN' descricao='Um sistema capaz de reunir e verificar automaticamente os dados relativos à pandemia de Covid-19 oriundos de diferentes fontes, gerando maior qualidade e confiabilidade das informações utilizadas pelas secretarias de sáude do estado no tocante à Vigilância Epidemiológica. Atuo como dev front-end web.'/>
@@ -184,15 +184,40 @@ return (
                 >
                     <h3>Projetos</h3>
             </motion.div>
-            {/* <div id='container-trabalhos'>
-                <a href="https://includee2023-1-qx1h.vercel.app/" target='_blank'>
-                    <CardTrabalhosProjetos caminhoImagem={'/include.jpg'} titulo='INCLUDE UERN Natal' descricao='O INCLUDE é um evento de inclusão aos colouros do curso de computação na UERN Natal, foi me pedido que fizesse uma landing page para mostrar tudo o que seria abordado juntamente com informações sobre o local, data e etc, além de redirecionar a um formulário para inscrição. Aqui utilizei HTML5, CSS3, Bootstrap e Js.'/>
+            <motion.div id='container-trabalhos'
+            
+                initial={{ opacity: 0, y: 500 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{  duration: 0.5, delay: 0.5  }}
+            >
+                <a href="https://pokedex-chi-eight-74.vercel.app/#" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/pokedex.jpg'} titulo='Pokedex' descricao='Implementei uma pokedex para reforçar meus conhecimentos em consumir API, utilizei o fetch do JS para fazer o consumo dos dados, implementei um filtro por nome e tipo de pokemon e utilizei a própria API do pokemon. Tecs utilizadas: HTML5, CSS3, JS e Bootstrap.'/>
                 </a>
 
-                <a href="https://clinica-prototipo2.vercel.app/" target='_blank'>
-                    <CardTrabalhosProjetos caminhoImagem={'/include.jpg'} titulo='FreeLancer para a Synapsismkt' descricao='Uma lading page 100% responsiva para captar o cliente para um clínica médica, o projeto só foi 70% finalizado, já foi totalmente aprovado pelo cliente porém falta o mesmo enviar os textos e imagens que ele deseja, porém mesmo assim já bateu 80/100 de SEO. Aqui utilizei HTML5 (semântico como sempre), CSS3, JS, Tailwind CSS, e NextJS.'/>
+                <a href="https://plataforma-aac-23.vercel.app/" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/arqAvan.jpg'} titulo='Plataforma AAC' descricao='Esse projeto consistiu em fazer uma plataforma para divulgação e cadastro no evento (EATEC) que foi realizado na UERN Natal com os cursos de tecnologia. Tecs utilizadas: HTML5, CSS3, JS e Bootstrap.'/>
                 </a>
-            </div> */}
+
+                <a href="https://hackathonuern22.vercel.app/" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/haka.jpg'} titulo='Hakaton UERN Natal' descricao='O Hakaton é um evento que tem como objetivo promover desafios e premiação aos alunos, a web page faz o cadastro dos participantes através de um formulário e monta os times. Tecs utilizadas: HTML5, CSS3, ReactJS.'/>
+                </a>
+
+                <a href="https://pizzaria-one.vercel.app/" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/pizzaria.jpg'} titulo='Pizzaria Lapetitozaa' descricao='Essa web page desenvolvi na disciplina de banco de dados, o intuito era simular um cadastro de cliente a inserir os dados através da web page. Tecs utilizadas: HTML5, CSS3, JS e Bootstrap.'/>
+                </a>
+
+                <a href="https://cordel-site.vercel.app/" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/cordel.jpg'} titulo='Cordel Moderno' descricao='Aqui o intuito foi colocar o cordel feito por Milton Duarte de uma forma diferenciada, o objetivo final foi realmente praticar e aprender mais sobre front, fortalecer os conhecimento. Tecs utilizadas: HTML5 e CSS3.'/>
+                </a>
+
+                <a href="https://bootstrap-alura-estudos.vercel.app/" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/fruta.jpg'} titulo='Fruta e Fruto' descricao='Implementando uma lading page de receitas. Tecs utilizadas: HTML5, CSS3 e Bootstrap.'/>
+                </a>
+
+                <a href="https://desafio-coor-lab-zeta.vercel.app/" target='_blank'>
+                    <CardTrabalhosProjetos caminhoImagem={'/desafio.jpg'} titulo='Desafio Melhor Frete' descricao='Esse foi um desafio para um processo seletivo que participei, onde pedia para consumir uma API disponibilizada por eles usando o AXIOS. O objetivo era construir o front, captar os dados de localidade e peso e trazer o resultado do frete mais barato e o frete mais rápido. Tecs utilizadas: VueJS, CSS3, HTML5, JS.'/>
+                </a>
+            </motion.div>
         </section>
     </>
   )
